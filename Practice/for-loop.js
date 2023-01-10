a27017d16e2e9d8773ff4d478c5451cedd142bd3
AzureAD\KarthikYelisetti <karthik.yelisetti@veeva.com>
2023-01-10 15:24:56 +0530

    updated javascript practice files

--- Practice/for-loop.js
+++
@@ -1,9 +0,0 @@
-for(var i=0;i<=10;i=i+2) {
-    console.log(i);
-}
-
-var i=0;
-while(i<=10) {
-    console.log(i);
-    i=i+2;
-}
--- Practice/multiple.js
+++
@@ -1,5 +0,0 @@
-const prompt = require("prompt-sync")();
-num = prompt("Enter a number")
-for (var i=1;i<=5;i++) {
-    console.log(num * i);
-}
