783269f02bd8cfc1c0245a8473d26866fa87e48b
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-03-05 19:33:43 +0530

    Updating the fetch using ajax code

--- Advanced/Fetch/script.js
+++ Advanced/Fetch/script.js
@@ -5,21 +5,42 @@ var getjson = document.getElementById("get-json");
 getjson.addEventListener("click", getJSON);
 
 function getText() {
-    fetch("file.txt")
-    .then(res => res.text())
-      .then(data => {
-        console.log(data);
-        // document.getElementById("empty").innerHTML = data;
+  document.getElementById("empty").innerHTML = "</br></br>";
+  fetch("file.txt")
+    .then((res) => res.text())
+    .then((data) => {
+      // console.log(data);
+      document.getElementById("empty").innerHTML = data;
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 }
 
 function getJSON() {
-    fetch("file.json")
-    .then(res => JSON.parse(res))
-      .then(data => {
-        console.log(data);
-        // document.getElementById("empty").innerHTML = data;
+  document.getElementById("empty").innerHTML = "</br></br>";
+  fetch("file.json")
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data);
+      let x = "";
+      data.forEach((object) => {
+        // console.log(object.body);
+        x += `<li>${object.body}</li>`;
+      });
+      document.getElementById("empty").innerHTML = x;
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
+}
+
+function getAPI() {
+  document.getElementById("empty").innerHTML = "</br></br>";
+  fetch("https://api.github.com/users")
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data);
+      let x = "";
+      data.forEach((user) => {
+        x += `<li>${user.login}</li>`;
+      });
+      document.getElementById("empty").innerHTML = x;
+    });
 }
