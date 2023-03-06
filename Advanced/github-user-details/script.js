43b387f8fb1ee71afb7e1615504f7c03763f11c6
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-03-06 11:27:49 +0530

    Updated the git hub user details code

--- Advanced/github-user-details/script.js
+++ Advanced/github-user-details/script.js
@@ -2,14 +2,28 @@ var gettext = document.getElementById("search-btn");
 gettext.addEventListener("click", getUserDetails);
 
 function getUserDetails() {
-    var userName = document.getElementById("search").value;
-    // console.log(userName);
-    fetch(`https://api.github.com/users/${userName}`)
-      .then((res) => res.json())
-      .then((data) => {
-        let x = "";
-        var main = document.getElementById("main");
-        x += `<div><img src=${data.avatar_url} /></div>`;
-        main.innerHTML = x;
-      });
-  }
+  var userName = document.getElementById("search-text").value;
+  fetch(`https://api.github.com/users/${userName}`)
+    .then((res) => res.json())
+    .then((data) => {
+      let x = "";
+      var main = document.getElementById("main");
+      x += `<div class="col-md-4 justify-content-right"><img src=${data.avatar_url} id="profile-img" /></div>`;
+      x += `<div class="col-md-4">
+              <p id="user-name">${data.name}</p>
+              <p id="user-data">
+                <ul id="details">
+                  <li>12 Followers </li>
+                  <li>24 Following </li>
+                  <li>${data.public_repos} Repos</li>
+                </ul>
+              </p>
+              <p id="user-repo">
+                <ul>
+                  <li></li>
+                </ul>
+              </p>
+            </div>`;
+      main.innerHTML = x;
+    });
+}
