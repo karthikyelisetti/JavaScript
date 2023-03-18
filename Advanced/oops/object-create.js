99ad9c990d42a6822f39a3cf29aa807d8eadd56b
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-03-18 18:28:26 +0530

    created prototype and object creation logic

---
+++ Advanced/oops/object-create.js
@@ -0,0 +1,14 @@
+const learner = {
+    active: true,
+    details: function(){
+        console.log(`Hello ${this.name}`)
+    }
+}
+
+const obj1 = Object.create(learner);
+console.log(obj1.active)
+
+obj1.active = false;
+console.log(obj1.active);
+obj1.name = "Karthik"
+obj1.details();
